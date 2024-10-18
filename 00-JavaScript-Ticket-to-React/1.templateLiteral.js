6e9119934d376468a81bb62fbc1067119945511b
Suyash-Kamath <110880307+Suyash-Kamath@users.noreply.github.com>
2024-10-18 10:59:19 +0530

    Create 1.templateLiteral.js

---
+++ 00-JavaScript-Ticket-to-React/1.templateLiteral.js
@@ -0,0 +1,15 @@
+const age = 20;
+const fullName = "John Doe"
+
+
+console.log("Hello Mr. "+fullName+" and your age is "+age);
+
+console.log(`Hello Mr. ${fullName} and your age is ${age}`);
+
+const a=12;
+const b=13
+console.log("Sum of the two numbers is "+a+b); // it converts into the string , string +integer= string
+
+console.log(`Sum of the two given numbers is ${a+b}`);
+
+
