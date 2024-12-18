0e12b45343741dc5644fe7e81bca0fc3f77a7b25
Suyash-Kamath <110880307+Suyash-Kamath@users.noreply.github.com>
2024-12-18 15:02:17 +0530

    Create eight.js

---
+++ 27-Filter-Map-Reduce/eight.js
@@ -0,0 +1,38 @@
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0);
+
+// Whatever initial value is given  , first time it goes into accumulator
+// when the loop goes for the first time , value of accumulator is 0 , it happens for only 1 time
+// next time the result of acc+curr goes into acc.
+
+console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
